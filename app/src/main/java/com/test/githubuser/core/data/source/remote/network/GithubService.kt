18c51aba2f2d2033aacbe63cbb5aa9d531ced5fb
linggakusuma <asesinomurder@gmail.com>
2021-06-01 11:31:30 +0700

    remove token

--- app/src/main/java/com/test/githubuser/core/data/source/remote/network/GithubService.kt
+++ app/src/main/java/com/test/githubuser/core/data/source/remote/network/GithubService.kt
@@ -1,12 +1,12 @@
 package com.test.githubuser.core.data.source.remote.network
 
-import com.test.githubuser.BuildConfig
 import com.test.githubuser.core.data.source.remote.response.BaseResponse
 import com.test.githubuser.core.data.source.remote.response.UserResponse
-import retrofit2.http.*
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GithubService {
-    @Headers("Authorization: ${BuildConfig.TOKEN_GITHUB}")
+
     @GET("search/users")
     suspend fun getUser(
         @Query("q") username: String,
